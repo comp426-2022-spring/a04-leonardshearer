4534846a449fdd74ee1beb0229fae37d0668a3b3
Leonard Shearer <leojshe@unc.edu>
2022-04-14 02:30:59 -0400

    Log db

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ if (args.help || args.h) {
 const HTTP_PORT = (1 <= args.port && args.port <= 65535) ? args.port : 5555
 const server = app.listen(HTTP_PORT, () => {
     console.log('App listening on port %PORT%'.replace('%PORT%', HTTP_PORT))
-});
+})
 
 const logging = (req, res, next) => {
     const query = db.prepare(`
@@ -54,38 +54,47 @@ app.use(logging)
 app.get('/app', (req, res) => {
     res.type('text/plain')
     res.status(200).end('OK')
-});
+})
 
 app.get('/app/flip', (req, res) => {
     res.type('application/json')
     res.status(200).json({ 'flip': coin.coinFlip() })
-});
+})
 
 app.get('/app/flips/:number', (req, res) => {
     const raw = coin.coinFlips(parseInt(req.params.number))
     const summary = coin.countFlips(raw)
     res.type('application/json')
     res.status(200).json({ 'raw': raw, 'summary': summary })
-});
+})
 
 app.get('/app/flip/call/heads', (req, res) => {
     res.type('application/json')
     res.status(200).json(coin.flipACoin('heads'))
-});
+})
 
 app.get('/app/flip/call/tails', (req, res) => {
     res.type('application/json')
     res.status(200).json(coin.flipACoin('tails'))
-});
+})
 
 if (args['debug'] === 'true') {
     app.get('/app/log/access', (req, res) => {
+        try {
+            const query = db.prepare('SELECT * from accesslog').all()
+            res.status(200).json(query)
+        } catch {
+            console.error(e)
+        }
+    })
 
-    });
+    app.get('/app/error', (req, res) => {
+        throw new Error('Error test successful.')
+    })
 }
 
 app.use(function (req, res) {
     res.type('text/plain')
     res.status(200).status(404).end('404 NOT FOUND')
-});
+})
 
